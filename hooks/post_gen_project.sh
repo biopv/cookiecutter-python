f54fe63460faf0e96716c2bba3ce6664afec4615
Petr <petr.volkov@mshareservices.es>
2024-10-11 10:28:37 +0200

    Changed hardcoded pyproject toml into a default poetry generation

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
+echo $(pwd)
+
+poetry init \
+       --name '{{ cookiecutter.project_name }}' \
+       --description '{{ cookiecutter.description }}' \
+       --author '{{ cookiecutter.author }}' \
+       --license MIT \
+       --python "^3.12" \
+       --dependency "pydantic" \
+       --dependency "typer" \
+       --dependency "loguru" \
+       --dependency "pydantic" \
+       --dev-dependency "mkdocs" \
+       --dev-dependency "mkdocs-material" \
+       --dev-dependency "pytest-sugar" \
+       --dev-dependency "mypy" \
+       --dev-dependency "pytest" \
+       --no-interaction
+
+cat << EOF >> pyproject.toml
+
+[tool.pytest.ini_options]
+pythonpath = ["src"]
+log_cli = "true"
+EOF
+
 poetry install
-
 git init
-
 poetry run pre-commit install
